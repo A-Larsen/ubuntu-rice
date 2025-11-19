764bbaab626bf5e9b483696dbb770a3f98e544b9
A-Larsen <austin.larsen@mailfence.com>
2025-11-19 10:15:25 -0500

    new scripts for dwm

--- home/.config/dwmblocks/blocks.def.h
+++ home/.config/dwmblocks/blocks.def.h
@@ -2,8 +2,7 @@
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	{" Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g", 30, 0},
-	{"volume: ", "echo $(pactl get-sink-volume @DEFAULT_SINK@ | grep -Po '\\d+(?=%)' | head -n 1) $(pactl get-sink-mute @DEFAULT_SINK@)", 1, 0},
-	// {"", "pactl get-sink-mute @DEFAULT_SINK@", 5, 0},
+	{"volume: ", "echo $(pactl get-sink-volume @DEFAULT_SINK@ | grep -Po '\\d+(?=%)' | head -n 1) $(pactl get-sink-mute @DEFAULT_SINK@)", 0, 17},
 	{"", "date '+%b %d (%a) %I:%M%p'", 5, 0},
 	{"", "echo \"$USER \"", 0, 0},
 };
